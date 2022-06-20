e70ddb5572f1fa0a12f34abb23be7cb9fdeef675
DESKTOP-JC48PGF <fanweihua@kyee.com.cn>
2022-06-21 00:12:41 +0800

    初æ¬提交

---
+++ src/main/java/Test01.java
@@ -0,0 +1,14 @@
+/**
+ * <pre>
+ * 任务：
+ * 描述：(这里用一句话描述这个类的作用)
+ * 作者：
+ * 时间：
+ * </pre>
+ */
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("hello leetcode");
+    }
+}
